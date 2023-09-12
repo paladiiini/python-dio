ed3ad3d056e366dd0404f114bedba6b1f2fd2b9f
paladiini <paladinivitor.vp@gmail.com>
2023-09-12 10:42:05 -0300

    Up

---
+++ strings_fatiamento.py
@@ -0,0 +1,16 @@
+nome = "    vitor"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+print(nome.capitalize())
+print(nome.strip())
+
+menu = "Python"
+
+print(menu.center(20, "-"))
+print("-".join(menu))
+
+profissao = "QA"
+
+print(Meu nome é {nome} e minha profissão é {profissao})
